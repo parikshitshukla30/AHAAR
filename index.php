84e4e9764e0ff15d9227c4e452c7672398c1b668
Parikshit Shukla <parikshitshukla30@gmail.com>
2026-03-03 09:14:24 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,105 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Zero Food Waste</title>
 
+<link rel="stylesheet" href="/zz final food/css/style.css">
+<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600;700&display=swap" rel="stylesheet">
+</head>
+
+<body>
+
+<header class="navbar">
+  <div class="logo">AHAAR</div>
+  <nav>
+    <a href="index.php">Home</a>
+    <a href="donor registration.php">Donor Register</a >
+    <a href="ngo registration.php">NGO Register</a>
+    <a href="donate.php">Donate Food</a>
+    <a href="dashboard.php">Track Donation</a>
+  </nav> 
+</header>
+
+<section class="hero">
+  <div class="overlay"></div>
+
+  <div class="hero-content">
+    <h1>AHAAR <h1> 
+<h2>Action for Hunger <br>And <br> Aid Relief <h2>
+    <p> <i>You Extra Food Will <br>Give Extra Happiness<br> On someone's face</i></p>
+
+    <a href="donate.php" class="btn">Donate Food</a>
+    <nmsp>
+    <a href="staff.php" class="btn-outline">Join Network</a>
+  </div>
+</section>
+
+<section class="features">
+  <div class="card">Prevent Food Waste</div>
+  <div class="card">Help NGOs</div>
+  <div class="card">Feed Animals</div>
+  <div class="card">♻ Protect Environment</div>
+  <div class="card">  
+   <p>  “If you can’t feed a hundred people, then feed just one.”
+    <br> अगर आप सौ लोगों को खाना नहीं खिला सकते,<br>
+तो कम से कम एक को ही खिला दीजिए।
+  <br> <br>— Mother Teresa</p>
+  </div>
+  <div class="card">
+<p>“The war against hunger is truly mankind’s war of liberation.”
+<br>भूख के खिलाफ लड़ाई, मानवता की सबसे बड़ी आज़ादी की लड़ाई है।
+<br> <br> — John F. Kennedy</p>
+</div>
+<div class="card">
+<p>“Hunger is not a problem. It is an obscenity. How wonderful it is that nobody need wait a single moment before starting to improve the world.”
+<br>भूख एक समस्या नहीं, बल्कि मानवता पर कलंक है।
+<br> <br> — Anne Frank</p>
+</div>
+  <div class="card">  
+    <p> “There are people in the world so hungry, that God cannot appear to them except in the form of bread.” <br> दुनिया में कुछ लोग इतने भूखे हैं कि
+ <br> उन्हें भगवान भी रोटी के रूप में ही दिखाई देते हैं।
+  <br> <br>— Mahatma Gandhi </p>
+  </div>
+
+  <div class="card">  
+    <p> 
+  “यज्ञशिष्टाशिनः सन्तो मुच्यन्ते सर्वकिल्बिषैः।”<br>
+(अध्याय 3, श्लोक 13) <br><br>
+अर्थ :- जो लोग भोजन को बाँटकर खाते हैं,<br> 
+वे सभी पापों से मुक्त हो जाते हैं।<br><br>
+— भगवद गीता</p>
+  </div>
+
+  <div class="card">
+<p>
+“और वे (अल्लाह के नेक बंदे) भोजन से प्रेम होने के बावजूद<br>
+निर्धन, अनाथ और बंदी को खिलाते हैं।”<br>
+(सूरह अल-इंसान 76:8)<br><br>
+—  क़ुरआन</p>
+</div>
+
+<div class="card"> 
+<p> 
+“ਵੰਡ ਛਕੋ” (वंड छको)<br>
+अर्थ:<br>
+जो है उसे बाँटकर खाओ<br> यही सच्चा धर्म है।
+<br><br>
+— गुरु ग्रंथ साहिब<br>
+</p>
+</div>
+
+<div class="card"> 
+<p>
+“भूखे को भोजन देना सबसे बड़ा पुण्य है।”<br><br>
+—  धम्मपद<br></p>
+</div>
+</section>
+
+<footer>
+  © 2026 AHAAR
+</footer>
+
+</body>
+</html>
